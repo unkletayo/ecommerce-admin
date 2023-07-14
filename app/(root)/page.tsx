abe2ae2ff75001950e272f1fa77d2d58999e5e33
Adetayo <akinsanyaadetayo@gmail.com>
2023-07-14 22:30:35 +0100

    Authenticated routes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from '../../components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 export default function SetupPage() {
   return (
     <div>
-      <p>Hello</p>
-      <Button size="icon">Click ME</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
